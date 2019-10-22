5f460e0ebced07061dff7cba657d5ed63d9954b5
Junghoon Yoon <juhuyoon@yahoo.com>
2019-10-22 11:32:42 -0400

    initial push to invoice branch

---
+++ game-store-invoice-server/src/main/java/com/company/gamestoreinvoiceserver/GameStoreInvoiceServerApplication.java
@@ -0,0 +1,13 @@
+package com.company.gamestoreinvoiceserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameStoreInvoiceServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameStoreInvoiceServerApplication.class, args);
+	}
+
+}
