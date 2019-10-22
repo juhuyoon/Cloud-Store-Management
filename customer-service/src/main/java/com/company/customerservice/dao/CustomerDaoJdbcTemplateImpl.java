a38748c600782c070eea18ac0c6af5eb2a65bd1f
BarbaraHernandez <bandezanne@gmail.com>
2019-10-22 13:45:49 -0400

    DAO tests running red

--- customer-service/src/main/java/com/company/customerservice/dao/CustomerDaoJdbcTemplateImpl.java
+++ customer-service/src/main/java/com/company/customerservice/dao/CustomerDaoJdbcTemplateImpl.java
@@ -1,4 +1,34 @@
 package com.company.customerservice.dao;
 
-public class CustomerDaoJdbcTemplateImpl {
+import com.company.customerservice.model.Customer;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class CustomerDaoJdbcTemplateImpl implements CustomerDao{
+    @Override
+    public Customer createCustomer(Customer customer) {
+        return null;
+    }
+
+    @Override
+    public Customer getCustomer(int customer_id) {
+        return null;
+    }
+
+    @Override
+    public List<Customer> getAllCustomers() {
+        return null;
+    }
+
+    @Override
+    public void updateCustomer(Customer customer) {
+
+    }
+
+    @Override
+    public void deleteCustomer(int customer_id) {
+
+    }
 }
--- customer-service/src/main/java/com/company/customerservice/service/ServiceLayer.java
+++ customer-service/src/main/java/com/company/customerservice/service/ServiceLayer.java
@@ -1,4 +1,5 @@
 package com.company.customerservice.service;
 
 public class ServiceLayer {
+
 }
