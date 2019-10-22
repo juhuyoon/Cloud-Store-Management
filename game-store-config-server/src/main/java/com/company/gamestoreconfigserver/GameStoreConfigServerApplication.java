5cbe18642971b6baec33c889b38b4d9098abd044
Junghoon Yoon <juhuyoon@yahoo.com>
2019-10-22 11:19:51 -0400

    add configuration of config and eureka ;

---
+++ game-store-config-server/src/main/java/com/company/gamestoreconfigserver/GameStoreConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.company.gamestoreconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameStoreConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameStoreConfigServerApplication.class, args);
+	}
+
+}
---
+++ game-store-eureka-server/src/main/java/com/company/gamestoreeurekaserver/GameStoreEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.company.gamestoreeurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameStoreEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameStoreEurekaServerApplication.class, args);
+	}
+
+}
