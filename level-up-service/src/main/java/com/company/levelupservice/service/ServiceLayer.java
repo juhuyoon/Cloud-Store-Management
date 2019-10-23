5c24780a40c346e57acce41e594a1fccac713dc5
BarbaraHernandez <bandezanne@gmail.com>
2019-10-23 13:24:10 -0400

    complete service layer tests, running red

--- level-up-service/src/main/java/com/company/levelupservice/service/ServiceLayer.java
+++ level-up-service/src/main/java/com/company/levelupservice/service/ServiceLayer.java
@@ -1,4 +1,37 @@
 package com.company.levelupservice.service;
 
+import com.company.levelupservice.dao.LevelUpDao;
+import com.company.levelupservice.model.LevelViewModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
 public class ServiceLayer {
+
+    private LevelUpDao levelUpDao;
+
+    @Autowired
+    public ServiceLayer(LevelUpDao levelUpDao) {
+        this.levelUpDao = levelUpDao;
+    }
+
+    public LevelViewModel addLevelUpEntry(LevelViewModel lvm){
+        return null;
+    }
+
+    public LevelViewModel findLevelUpEntry(int level_up_id){
+        return null;
+    }
+
+    public List<LevelViewModel> findAllLevelUpEntries(){
+        return null;
+    }
+
+    public void updateLevelUpEntry(LevelViewModel lvm){
+
+    }
+
+    public void removeLevelUpEntry(int level_up_id){}
 }
