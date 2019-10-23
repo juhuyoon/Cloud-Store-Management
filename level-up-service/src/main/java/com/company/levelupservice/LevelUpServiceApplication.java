00c2273fc547c945c78861a434b8f889244f4bef
BarbaraHernandez <bandezanne@gmail.com>
2019-10-23 11:05:22 -0400

    dao tests complete, running red

--- level-up-service/src/main/java/com/company/levelupservice/LevelUpServiceApplication.java
+++ level-up-service/src/main/java/com/company/levelupservice/LevelUpServiceApplication.java
@@ -2,10 +2,12 @@ package com.company.levelupservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableCircuitBreaker
 public class LevelUpServiceApplication {
 
 	public static void main(String[] args) {
---
+++ level-up-service/src/main/java/com/company/levelupservice/dao/LevelUpDaoJdbcTemplateImpl.java
@@ -0,0 +1,32 @@
+package com.company.levelupservice.dao;
+
+import com.company.levelupservice.model.LevelUp;
+
+import java.util.List;
+
+public class LevelUpDaoJdbcTemplateImpl implements LevelUpDao{
+    @Override
+    public LevelUp createLevelUp(LevelUp lvl) {
+        return null;
+    }
+
+    @Override
+    public LevelUp readLevelUp(int level_up_id) {
+        return null;
+    }
+
+    @Override
+    public List<LevelUp> readAll() {
+        return null;
+    }
+
+    @Override
+    public void updateLevelUp(LevelUp lvl) {
+
+    }
+
+    @Override
+    public void deleteLevelUp(int level_up_id) {
+
+    }
+}
