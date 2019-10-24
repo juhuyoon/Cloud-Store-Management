03993409e11393d8906be7371263fefd802f95ed
BarbaraHernandez <bandezanne@gmail.com>
2019-10-24 11:36:36 -0400

    added new route

--- level-up-service/src/main/java/com/company/levelupservice/controller/LevelUpController.java
+++ level-up-service/src/main/java/com/company/levelupservice/controller/LevelUpController.java
@@ -1,12 +1,12 @@
 package com.company.levelupservice.controller;
 
+import com.company.levelupservice.circuitbreaker.LevelUpServiceCircuitBreaker;
 import com.company.levelupservice.model.LevelViewModel;
 import com.company.levelupservice.service.ServiceLayer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
-import javax.xml.ws.Service;
 import java.util.List;
 
 @RestController
@@ -15,6 +15,9 @@ public class LevelUpController {
 
     @Autowired
     private ServiceLayer serviceLayer;
+
+    @Autowired
+    private LevelUpServiceCircuitBreaker circuitBreaker;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
@@ -26,6 +29,7 @@ public class LevelUpController {
     @ResponseStatus(HttpStatus.FOUND)
     public List<LevelViewModel> getAllLevelUp(){
         return serviceLayer.findAllLevelUpEntries();
+
     }
 
     @GetMapping(value = "/{level_up_id}")
@@ -33,6 +37,14 @@ public class LevelUpController {
     public LevelViewModel getLevelUp(@PathVariable int level_up_id){
         return serviceLayer.findLevelUpEntry(level_up_id);
     }
+
+    //not in spec: get level_up entry by customer_id
+    @GetMapping(value = "/customer/{customer_id}")
+    @ResponseStatus(HttpStatus.FOUND)
+    public LevelViewModel getLevelUpByCustomer(@PathVariable int customer_id){
+        return null;
+    }
+
 
     @PutMapping(value = "/{level_up_id}")
     @ResponseStatus(HttpStatus.OK)
