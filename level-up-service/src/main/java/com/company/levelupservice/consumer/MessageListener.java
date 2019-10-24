f78696d1935f90ccd64fc3a4b73af63d90e3b1cf
BarbaraHernandez <bandezanne@gmail.com>
2019-10-24 10:11:57 -0400

    added queue consumer

--- level-up-service/src/main/java/com/company/levelupservice/consumer/MessageListener.java
+++ level-up-service/src/main/java/com/company/levelupservice/consumer/MessageListener.java
@@ -1,19 +1,24 @@
 package com.company.levelupservice.consumer;
 
-import com.company.levelupservice.model.LevelUp;
+import com.company.levelupservice.LevelUpServiceApplication;
+import com.company.levelupservice.controller.LevelUpController;
 import com.company.levelupservice.model.LevelViewModel;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MessageListener {
 
-        @RabbitListener(queues = CommentQueueConsumerApplication.QUEUE_NAME)
+    @Autowired
+    private LevelUpController controller;
+
+        @RabbitListener(queues = LevelUpServiceApplication.QUEUE_NAME)
         public void receiveMessage(LevelViewModel lvm) {
             if (lvm.getLevel_up_id() > 0) {
-                client.updateComment(cmt);
+                controller.updateLevelUp(lvm);
             } else {
-                client.createComment(cmt);
+                controller.createLevelUp(lvm);
             }
         }
 }
